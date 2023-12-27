56ea3d0ea058475b0d684bed9ce5952b7414260c
Akash kumar <80808362+akashkumar220011@users.noreply.github.com>
2023-12-27 17:27:56 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 exports.db = () => {
-    mongoose.connect("mongodb://localhost:27017/servey")
+    mongoose.connect("mongodb+srv://krakash1022:0987@cluster0.ktvqi8r.mongodb.net/servey")
     .then((e) => console.log(`Connected to Mongodb: ${e.connection.host}`))
     .catch((e) => console.log(e));
 };
